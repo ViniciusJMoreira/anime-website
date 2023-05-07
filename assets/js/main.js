6557b05ea9826ef7ff3005fea8d68f4d26dd50e2
Vinicius <viniciusit.moreira@gmail.com>
2023-05-07 18:02:42 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,29 @@
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+const navMenu = document.getElementById('nav-menu')
+function showMenu () {
+  navMenu.style.setProperty('right', '0')
+}
+const navToggle = document.getElementById('nav-toggle')
+navToggle.addEventListener('click', showMenu)
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+const navLink = document.querySelectorAll('.nav-link')
+function hiddenMenu() {
+  navMenu.style.setProperty('right', '-100%')
+}
+function activeLink() {
+  navLink.forEach((link) =>
+  link.classList.remove('active-link'))
+  this.classList.add('active-link')
+}
+const navClose = document.getElementById('nav-close')
+navClose.addEventListener('click', hiddenMenu)
+navLink.forEach((item) => {
+  item.addEventListener('click', hiddenMenu)
+  item.addEventListener('click', activeLink)
+})
 /*=============== SCROLL HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById('header')
